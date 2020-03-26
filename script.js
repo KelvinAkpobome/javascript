007faf10f4a11314c6c1a443cbfe2b631660f92e
kelvinakpobome <kelvinakpobome@gmail.com>
2020-03-26 16:59:26 +0100

    add commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var myName = "Akpobome, Oke Kelvin";
+var kelvinCourses = [ "html", "CSS", "JS", "UI/UX", "Flutter", "Go", "kotlin", "Python", "Figma"];
+console.log(myName)
+console.log(kelvinCourses)
+
+//condition to check if course length is even or odd
+
+for (var i = 0; i <=200; i++){
+    if ( i % 2 == 1){
+        console.log(i);
+    }
+}
